4253e6851c5567e158e8c178fa95d2cd838a0fa4
Sanzhar <semid46@gmail.com>
2022-01-10 16:00:54 +0600

    fist trying to create and connect to the database

--- src/main/java/dbsqlite/DataBase.java
+++ src/main/java/dbsqlite/DataBase.java
@@ -1,32 +1,18 @@
 package dbsqlite;
 
-import java.sql.Connection;
-import java.sql.DatabaseMetaData;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class DataBase {
-    public static void connect() {
-        Connection conn = null;
-        try {
-            // db parameters
-            String url = "jdbc:sqlite:users.sqlite";
-            // create a connection to the database
-            conn = DriverManager.getConnection(url);
+    private static String path = "posts.sqlite";
 
-            System.out.println("Connection to SQLite has been established.");
-
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        } finally {
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException ex) {
-                System.out.println(ex.getMessage());
-            }
-        }
+    public static void main(String[] args) {
+//        createNewDatabase(path);
+//        createNewTable();
+        DataBase app = new DataBase();
+        // insert three new rows
+        app.insert("Raw Materials", 3000);
+        app.insert("Semifinished Goods", 4000);
+        app.insert("Finished Goods", 5000);
     }
 
     public static void createNewDatabase(String fileName) {
@@ -38,6 +24,60 @@ public class DataBase {
                 System.out.println("The driver name is " + meta.getDriverName());
                 System.out.println("A new database has been created.");
             }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private Connection connect() {
+        // SQLite connection string
+        String url = "jdbc:sqlite:" + path;
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+
+    }
+
+    public static void createNewTable() {
+        // SQLite connection string
+        String url = "jdbc:sqlite:" + path;
+
+        // SQL statement for creating a new table
+        String sql = """
+                CREATE TABLE IF NOT EXISTS Users (
+                	id integer PRIMARY KEY,
+                	name text NOT NULL,
+                	capacity real
+                );""";
+
+        try (Connection conn = DriverManager.getConnection(url);
+             Statement stmt = conn.createStatement()) {
+            // create a new table
+            stmt.execute(sql);
+            System.out.println("executed");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    /**
+     * Insert a new row into the warehouses table
+     *
+     * @param name name
+     * @param capacity
+     */
+    public void insert(String name, double capacity) {
+        String sql = "INSERT INTO main.Users(name,capacity) VALUES(?,?)";
+
+        try (Connection conn = this.connect();
+             PreparedStatement prepareStatement = conn.prepareStatement(sql)) {
+            prepareStatement.setString(1, name);
+            prepareStatement.setDouble(2, capacity);
+            prepareStatement.executeUpdate();
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
